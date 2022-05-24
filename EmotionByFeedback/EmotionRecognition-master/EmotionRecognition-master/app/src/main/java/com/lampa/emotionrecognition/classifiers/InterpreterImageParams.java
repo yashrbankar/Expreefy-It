7cba9395d1974a42262041202c65491b701f65b0
yashrbankar <yashrbankar@gmail.com>
2022-05-24 22:49:48 +0530

    Comments are added

--- EmotionByFeedback/EmotionRecognition-master/EmotionRecognition-master/app/src/main/java/com/lampa/emotionrecognition/classifiers/InterpreterImageParams.java
+++ EmotionByFeedback/EmotionRecognition-master/EmotionRecognition-master/app/src/main/java/com/lampa/emotionrecognition/classifiers/InterpreterImageParams.java
@@ -5,31 +5,36 @@ import org.tensorflow.lite.Interpreter;
 // in which operations are executed for model inference.
 public class InterpreterImageParams 
 {
-    // Tensor indices of an image parameters
-    private final static int IMAGE_INPUT_TENSOR_INDEX = 0;
-    private final static int IMAGE_OUTPUT_TENSOR_INDEX = 0;
-
-    // Indices of an input image parameters
+    // Indices of an input image parameters set as 1 2 3
     private final static int MODEL_INPUT_WIDTH_INDEX = 1;
     private final static int MODEL_INPUT_HEIGHT_INDEX = 2;
     private final static int MODEL_INPUT_COLOR_DIM_INDEX = 3;
 
+    // Tensor indices of an image parameters set as 0
+    private final static int IMAGE_INPUT_TENSOR_INDEX = 0;
+    private final static int IMAGE_OUTPUT_TENSOR_INDEX = 0;
+
     // Index of an output result array
     private final static int MODEL_OUTPUT_LENGTH_INDEX = 1;
-
-    public static int getInputImageWidth(Interpreter interpreter) {
+    
+    // Image Width Tensorflow
+    public static int getInputImageWidth(Interpreter interpreter) 
+    {
         return interpreter.getInputTensor(IMAGE_INPUT_TENSOR_INDEX).shape()[MODEL_INPUT_WIDTH_INDEX];
     }
 
-    public static int getInputImageHeight(Interpreter interpreter) {
+    public static int getInputImageHeight(Interpreter interpreter) 
+    {
         return interpreter.getInputTensor(IMAGE_INPUT_TENSOR_INDEX).shape()[MODEL_INPUT_HEIGHT_INDEX];
     }
 
-    public static int getInputColorDimLength(Interpreter interpreter) {
+    public static int getInputColorDimLength(Interpreter interpreter) 
+    {
         return interpreter.getInputTensor(IMAGE_INPUT_TENSOR_INDEX).shape()[MODEL_INPUT_COLOR_DIM_INDEX];
     }
 
-    public static int getOutputLength(Interpreter interpreter) {
+    public static int getOutputLength(Interpreter interpreter) 
+    {
         return interpreter.getOutputTensor(IMAGE_OUTPUT_TENSOR_INDEX).shape()[MODEL_OUTPUT_LENGTH_INDEX];
     }
 }
