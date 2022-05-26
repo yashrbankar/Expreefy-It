80616f22691359d246eca279376e8d2844da43f3
yashrbankar <yashrbankar@gmail.com>
2022-05-26 11:29:51 +0530

    perf(Added sorting for the img ): Sorting of map
    According to the first highest value of the faceId
    map is get sorted
    
    Sorting Algorithm

--- EmotionByFeedback/EmotionRecognition-master/EmotionRecognition-master/app/src/main/java/com/lampa/emotionrecognition/utils/SortingHelper.java
+++ EmotionByFeedback/EmotionRecognition-master/EmotionRecognition-master/app/src/main/java/com/lampa/emotionrecognition/utils/SortingHelper.java
@@ -1,2 +1,26 @@
+package com.ExpreefyIT.FeedbackByEmtion.utils;
+// Latest Version
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
+public class SortingHelper {
+    // Generic function to sort Map in descending order by values
+    public static <K, V extends Comparable> Map<K, V> sortByValues(Map<K, V> map) {
+        List<Map.Entry<K, V>> mappings = new ArrayList<>(map.entrySet());
+        Collections.sort(mappings, (o1, o2) ->o1.getValue().compareTo(o2.getValue()));
+
+        // Create an empty LinkedHashMap that takes into account the insertion order of new elements
+        Map<K, V> linkedHashMap = new LinkedHashMap<>();
+
+        // Add entities in sorted order
+        for (Map.Entry<K, V> entry : mappings) {
+            linkedHashMap.put(entry.getKey(), entry.getValue());
+        }
+
+        return linkedHashMap;
+    }
+}
