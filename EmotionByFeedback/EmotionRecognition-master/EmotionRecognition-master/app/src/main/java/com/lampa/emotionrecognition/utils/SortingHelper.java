a70d1eb8eb20a09e018372cfe55f16c7d4d5b3c1
yashrbankar <yashrbankar@gmail.com>
2022-05-26 11:35:21 +0530

    docs(Comments added ): Sorting Algorithm

--- EmotionByFeedback/EmotionRecognition-master/EmotionRecognition-master/app/src/main/java/com/lampa/emotionrecognition/utils/SortingHelper.java
+++ EmotionByFeedback/EmotionRecognition-master/EmotionRecognition-master/app/src/main/java/com/lampa/emotionrecognition/utils/SortingHelper.java
@@ -6,11 +6,12 @@ import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-
+// Data Structure are used 
 public class SortingHelper {
-    // Generic function to sort Map in descending order by values
+    // function to sort Map in descending order by values of Emotion index
     public static <K, V extends Comparable> Map<K, V> sortByValues(Map<K, V> map) {
         List<Map.Entry<K, V>> mappings = new ArrayList<>(map.entrySet());
+        // sorting 
         Collections.sort(mappings, (o1, o2) ->o1.getValue().compareTo(o2.getValue()));
 
         // Create an empty LinkedHashMap that takes into account the insertion order of new elements
